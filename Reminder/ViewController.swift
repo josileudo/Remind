37e68f52f302d59df77ad00fd1dbb047bbd0eb6c
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2024-12-27 08:31:38 -0300

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Josileudo on 27/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
