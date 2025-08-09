b9e6cf6c5a350d982ff05bc1130a9d0a82fd72a7
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2025-08-09 16:28:02 -0300

    :sparkles: feat: Implement firebase library

---
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginBottomSheetViewModel.swift
+//  Reminder
+//
+//  Created by Josileudo on 09/08/25.
+//
+
+import Foundation
+
+class LoginBottomSheetViewModel {
+    func doAuth(usernameAuth: String, passwordAuth: String) {
+        print(<#T##items: Any...##Any#>)
+    }
+}
